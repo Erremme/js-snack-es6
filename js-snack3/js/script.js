f6fb8d20c4b526dd46691a9f8637a10e2deee4e6
Erremme <Erremme2.0@gmail.com>
2024-12-15 15:20:18 +0100

    feat: make third

--- js-snack3/js/script.js
+++ js-snack3/js/script.js
@@ -1,3 +1,24 @@
 /*Snack 3 (Bonus)
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.*/
+
+//FUNCTION 
+function getElementFromArray(array, a, b){
+const arrayElement =[];
+
+for(let i = a; i <= b; i++){
+    arrayElement.push(array[i])
+    
+}
+    return arrayElement;
+}
+
+
+//VARIABLES
+const club = ["Milan", "Inter", "Roma", "Atalanta", "Cagliari", "Lazio", "Venezia", "Juventus" ];
+const a = 3;
+const b = 6;
+
+const result = getElementFromArray(club, a, b )
+
+console.log(result)
